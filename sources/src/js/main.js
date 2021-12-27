22ea1f3f503269df5da0b6f4dece614bb31ee8d2
RxPro <wxweb@mail.ru>
2021-12-27 14:27:53 +0300

    Адаптивная верстка главной страницы

---
+++ sources/src/js/main.js
@@ -0,0 +1,48 @@
+"use strict";
+
+$(document).ready(function(){
+    photoSlider();
+    wishSlider();
+});
+
+function photoSlider() {
+    var swiper = new Swiper(".photo__slider-container", {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        loop: false,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".photo__next",
+        },
+        breakpoints: {
+            1366: {
+                spaceBetween: 30,
+            }
+        },
+    });
+}
+
+function wishSlider() {
+    var swiper = new Swiper(".wish__slider", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: false,
+        navigation: {
+            prevEl: ".wish__prev",
+            nextEl: ".wish__next",
+        },
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+        breakpoints: {
+            1280: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            }
+        },
+    });
+}
